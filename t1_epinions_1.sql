a763ac590fce3f5168f8b2bda634e6006343dc98
Ubuntu <ubuntu@ip-172-31-95-173.ec2.internal>
2022-03-16 06:08:05 +0000

    config changes

--- t1_epinions_1.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX idx_review_rating ON review(rating);
-CREATE INDEX idx_review_i_id ON review(i_id);
-CREATE INDEX idx_trust_source_u_id ON trust(source_u_id);
-CREATE INDEX idx_review_u_id ON review(u_id);
-CREATE INDEX idx_trust_target_u_id ON trust(target_u_id);
